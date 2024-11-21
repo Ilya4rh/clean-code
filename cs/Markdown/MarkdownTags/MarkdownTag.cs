2e50cd0748add892afce4d697c5fe954a004796f
Ilya4rh <chernykn12ilya12@gmail.com>
2024-11-21 18:52:09 +0500

    Добавил базовый класс для markdown тэгов

---
+++ cs/Markdown/MarkdownTags/MarkdownTag.cs
@@ -0,0 +1,23 @@
+﻿namespace Markdown.MarkdownTags;
+
+public abstract class MarkdownTag
+{
+    // Позиция тэга в строке
+    private int position;
+    
+    public MarkdownTag(int position)
+    {
+        this.position = position;
+    }
+
+    /// <summary>
+    /// Длина тэга
+    /// </summary>
+    public abstract int Length { get; }
+    
+    /// <summary>
+    /// Проверка валидности тэга
+    /// </summary>
+    /// <returns></returns>
+    public abstract bool IsValid(string markdownLine);
+}
