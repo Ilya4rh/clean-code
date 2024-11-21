80442536ff915a3be85a1089ca7e0ea92625894c
Ilya4rh <chernykn12ilya12@gmail.com>
2024-11-21 19:22:39 +0500

    Добавил класс и интерфейс конвертера

---
+++ cs/Markdown/Converters/HtmlConverter.cs
@@ -0,0 +1,11 @@
+﻿using Markdown.MarkdownTags;
+
+namespace Markdown.Converters;
+
+public class HtmlConverter : IConverter
+{
+    public string Convert(MarkdownTag markdownTag, string markdownLine)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ cs/Markdown/Converters/IConverter.cs
@@ -0,0 +1,8 @@
+﻿using Markdown.MarkdownTags;
+
+namespace Markdown.Converters;
+
+public interface IConverter
+{
+    string Convert(MarkdownTag markdownTag, string markdownLine);
+}
