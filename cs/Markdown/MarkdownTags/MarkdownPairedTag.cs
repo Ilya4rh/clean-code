feb41bea91cfa6ae610e9b987d4d9c6a250b9af3
Ilya4rh <chernykn12ilya12@gmail.com>
2024-11-23 12:51:26 +0500

    Добавил классы для одиночных и парных тэгов

---
+++ cs/Markdown/MarkdownTags/MarkdownPairedTag.cs
@@ -0,0 +1,16 @@
+﻿namespace Markdown.MarkdownTags;
+
+/// <summary>
+/// Парный markdown тэг
+/// </summary>
+public class MarkdownPairedTag : MarkdownTag
+{
+    public bool IsClosedTag;
+    
+    public MarkdownPairedTag(MarkdownTagType tagType, int position, bool isClosedTag = false) : base(tagType, position)
+    {
+        IsClosedTag = isClosedTag;
+    }
+
+    public override int Length { get; }
+}
---
+++ cs/Markdown/MarkdownTags/MarkdownSingleTag.cs
@@ -0,0 +1,13 @@
+﻿namespace Markdown.MarkdownTags;
+
+/// <summary>
+/// Одиночный markdown тэг
+/// </summary>
+public class MarkdownSingleTag : MarkdownTag
+{
+    public MarkdownSingleTag(MarkdownTagType tagType, int position) : base(tagType, position)
+    {
+    }
+
+    public override int Length { get; }
+}
