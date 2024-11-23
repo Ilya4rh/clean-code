067cd7ee14b43e622d0791fb8c3f6f76afda2e3e
Ilya4rh <chernykn12ilya12@gmail.com>
2024-11-23 12:47:39 +0500

    Добавил валидатор для найденных тэгов

---
+++ cs/Markdown/MarkdownTagValidators/IValidator.cs
@@ -0,0 +1,31 @@
+﻿using Markdown.MarkdownTags;
+
+namespace Markdown.MarkdownTagValidators;
+
+public interface IValidator
+{
+    /// <summary>
+    /// Проверяет валидность одиночного тэга
+    /// </summary>
+    /// <param name="tagType"> Тип тэга </param>
+    /// <param name="positionOnLine"> Позиция тэга в строке </param>
+    /// <param name="line"> Строка, в которой находится тэг </param>
+    /// <returns></returns>
+    bool IsValidSingleTag(MarkdownTagType tagType, int positionOnLine, string line);
+
+    /// <summary>
+    /// Проверяет валидность парного тэга
+    /// </summary>
+    /// <param name="tagType"> Тип тэга </param>
+    /// <param name="positionOpenTagOnLine"> Позиция открывающего тэга в строке </param>
+    /// <param name="positionCloseTagOnLine"> Позиция закрывающего тэга в строке </param>
+    /// <param name="line"> Строк, в которой находятся тэги </param>
+    /// <param name="externalTagType"> Тип внешнего тэга, для текущего </param>
+    /// <returns></returns>
+    bool IsValidPairedTag(
+        MarkdownTagType tagType, 
+        int positionOpenTagOnLine, 
+        int positionCloseTagOnLine, 
+        string line,
+        MarkdownTagType? externalTagType = null);
+}
---
+++ cs/Markdown/MarkdownTagValidators/MarkdownTagValidator.cs
@@ -0,0 +1,21 @@
+﻿using Markdown.MarkdownTags;
+
+namespace Markdown.MarkdownTagValidators;
+
+public class MarkdownTagValidator : IValidator
+{
+    public bool IsValidSingleTag(MarkdownTagType tagType, int positionOnLine, string line)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool IsValidPairedTag(
+        MarkdownTagType tagType, 
+        int positionOpenTagOnLine, 
+        int positionCloseTagOnLine, 
+        string line,
+        MarkdownTagType? externalTag = null)
+    {
+        throw new NotImplementedException();
+    }
+}
