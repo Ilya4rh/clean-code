ed86ee9ecf1955f029e20729f16a4dad587da520
Ilya4rh <chernykn12ilya12@gmail.com>
2024-12-01 12:11:19 +0500

    Добавил html конвертет с тестами

--- cs/Markdown/Converters/HtmlConverter.cs
+++ cs/Markdown/Converters/HtmlConverter.cs
@@ -1,4 +1,5 @@
-﻿using Markdown.MarkdownTags;
+﻿using System.Text;
+using Markdown.MarkdownTags;
 
 namespace Markdown.Converters;
 
@@ -6,6 +7,56 @@ public class HtmlConverter : IConverter
 {
     public string Convert(IEnumerable<MarkdownTag> tagsInLine, string markdownLine)
     {
-        throw new NotImplementedException();
+        var tagsSortingByPosition = tagsInLine
+            .OrderBy(tag => tag.Position)
+            .ToArray();
+
+        if (tagsSortingByPosition.Length == 0)
+            return markdownLine;
+        
+        var startIndexOfStringFragment = 0;
+        var htmlLine = new StringBuilder();
+        
+        foreach (var tag in tagsSortingByPosition)
+        {
+            var htmlTag = GetHtmlTag(tag);
+            
+            if (tag.Position == 0)
+            {
+                var positionNextChar = tag.Position + tag.Length;
+                startIndexOfStringFragment =
+                    tag.TagType == MarkdownTagType.Heading ? positionNextChar + 1 : positionNextChar;
+                htmlLine.Append(htmlTag);
+                continue;
+            }
+
+            var lineFragmentLength = tag.Position - startIndexOfStringFragment;
+            var lineFragment = markdownLine.AsSpan(startIndexOfStringFragment, lineFragmentLength);
+            htmlLine.Append(lineFragment).Append(htmlTag);
+            startIndexOfStringFragment = tag.Position + tag.Length;
+        }
+
+        if (startIndexOfStringFragment != markdownLine.Length)
+            htmlLine.Append(markdownLine.AsSpan(startIndexOfStringFragment,
+                markdownLine.Length - startIndexOfStringFragment));
+        
+        if (tagsSortingByPosition[0].TagType == MarkdownTagType.Heading)
+            htmlLine.Append(GetHtmlTag(tagsSortingByPosition[0], true));
+        
+        return htmlLine.ToString();
+    }
+
+    private static string GetHtmlTag(MarkdownTag markdownTag, bool isNeedClosingTagForSingle = false)
+    {
+        return markdownTag.TagType switch
+        {
+            MarkdownTagType.Heading when isNeedClosingTagForSingle => "\\</h1>",
+            MarkdownTagType.Heading => "\\<h1>",
+            MarkdownTagType.Bold when markdownTag.IsClosedTag => "\\</strong>",
+            MarkdownTagType.Bold => "\\<strong>",
+            MarkdownTagType.Italics when markdownTag.IsClosedTag => "\\</em>",
+            MarkdownTagType.Italics => "\\<em>",
+            _ => ""
+        };
     }
 }
--- cs/MarkdownTests/HtmlConverterTests.cs
+++ cs/MarkdownTests/HtmlConverterTests.cs
@@ -1,6 +1,107 @@
-﻿namespace MarkdownTests;
+﻿using FluentAssertions;
+using Markdown.Converters;
+using Markdown.MarkdownTags;
+using NUnit.Framework;
 
+namespace MarkdownTests;
+
+[TestFixture]
 public class HtmlConverterTests
 {
+    private IConverter converter;
+
+    [OneTimeSetUp]
+    public void OneTimeSetup()
+    {
+        converter = new HtmlConverter();
+    }
     
+    [Test]
+    public void Convert_ReturnOriginalString_WhenWereNoTags()
+    {
+        var markdownLine = "Здесь нет тегов";
+
+        var htmlLine = converter.Convert([], markdownLine);
+
+        htmlLine.Should().Be(markdownLine);
+    }
+    
+    [Test]
+    public void Convert_ShouldWrapLineWithHeadingTags_WhenHeadingTagProvided()
+    {
+        var markdownLine = "# Текст с заголовком";
+        var tags = new List<MarkdownTag>
+        {
+            new(MarkdownTagType.Heading, 0, 1)
+        };
+
+        var htmlLine = converter.Convert(tags, markdownLine);
+
+        htmlLine.Should().Be(@"\<h1>Текст с заголовком\</h1>");
+    }
+
+    [Test]
+    public void Convert_ShouldApplyBoldTag_WhenBoldTagProvided()
+    {
+        var markdownLine = "Текст с __полужирным__ тегом";
+        var tags = new List<MarkdownTag>
+        {
+            new(MarkdownTagType.Bold, 8, 2),
+            new(MarkdownTagType.Bold, 20, 2, true)
+        };
+
+        var htmlLine = converter.Convert(tags, markdownLine);
+
+        htmlLine.Should().Be(@"Текст с \<strong>полужирным\</strong> тегом");
+    }
+
+    [Test]
+    public void Convert_ShouldApplyItalicsTag_WhenItalicsTagProvided()
+    {
+        var markdownLine = "Текст с _курсивом_";
+        var tags = new List<MarkdownTag>
+        {
+            new(MarkdownTagType.Italics, 8, 1),
+            new(MarkdownTagType.Italics, 17, 1, true)
+        };
+
+        var htmlLine = converter.Convert(tags, markdownLine);
+
+        htmlLine.Should().Be(@"Текст с \<em>курсивом\</em>");
+    }
+
+    [Test]
+    public void Convert_ShouldHandleMultipleTags_WhenOneWordContainsAllTags()
+    {
+        var markdownLine = "# ___Привет___";
+        var tags = new List<MarkdownTag>
+        {
+            new(MarkdownTagType.Heading, 0, 1),
+            new(MarkdownTagType.Bold, 2, 2),
+            new(MarkdownTagType.Italics, 4, 1),
+            new(MarkdownTagType.Italics, 11, 1, true),
+            new(MarkdownTagType.Bold, 12, 2, true)
+        };
+        
+        var htmlLine = converter.Convert(tags, markdownLine);
+
+        htmlLine.Should().Be(@"\<h1>\<strong>\<em>Привет\</em>\</strong>\</h1>");
+    }
+
+    [Test]
+    public void Convert_ShouldHandleNestedTags_WhenItalicsTagsInBoldTags()
+    {
+        var markdownLine = "__Тут _курсив_ внутри полужирного__";
+        var tags = new List<MarkdownTag>
+        {
+            new(MarkdownTagType.Bold, 0, 2),
+            new(MarkdownTagType.Italics, 6, 1),
+            new(MarkdownTagType.Italics, 13, 1, true),
+            new(MarkdownTagType.Bold, 33, 2, true)
+        };
+        
+        var htmlLine = converter.Convert(tags, markdownLine);
+
+        htmlLine.Should().Be(@"\<strong>Тут \<em>курсив\</em> внутри полужирного\</strong>");
+    }
 }
