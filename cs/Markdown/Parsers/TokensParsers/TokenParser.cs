f00f7c9ebe73c77be03395752d07f5475635ec44
Ilya4rh <chernykn12ilya12@gmail.com>
2024-12-03 00:40:42 +0500

    Добавил парсер токенов

---
+++ cs/Markdown/Parsers/TokensParsers/TokenParser.cs
@@ -0,0 +1,27 @@
+﻿using Markdown.MarkdownTags;
+using Markdown.Tokens;
+using Markdown.Tokens.TagTokens;
+
+namespace Markdown.Parsers.TokensParsers;
+
+public class TokenParser : ITokensParser
+{
+    public IEnumerable<MarkdownTag> ParserMarkdownTags(IEnumerable<IToken> paragraphOfTokens)
+    {
+        var tagTokens = GetTagTokens(paragraphOfTokens).ToList();
+
+        for (var i = 0; i < tagTokens.Count; i++)
+        {
+             
+        }
+
+        return [];
+    }
+
+    private static IEnumerable<TagToken> GetTagTokens(IEnumerable<IToken> tokens)
+    {
+        return tokens
+            .Where(token => token.Type == TokenType.Tag)
+            .Select(token => (token as TagToken)!);
+    }
+}
