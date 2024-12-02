1a6aad9e980504659b831cc23ce7adda0712def1
Ilya4rh <chernykn12ilya12@gmail.com>
2024-12-02 19:53:19 +0500

    Добавил интерфейс для токенов и два основных класса для них

---
+++ cs/Markdown/Tokens/CommonTokens/CommonToken.cs
@@ -0,0 +1,19 @@
+﻿namespace Markdown.Tokens.CommonTokens;
+
+public abstract class CommonToken : IToken
+{
+    public abstract TokenType Type { get; }
+    public abstract string Content { get; }
+
+    public static CommonToken GetCommonToken(char symbol)
+    {
+        if (char.IsDigit(symbol))
+            return new DigitToken(symbol.ToString());
+        if (char.IsWhiteSpace(symbol))
+            return new SpaceToken();
+        if (symbol == '\\')
+            return new ScreeningToken();
+
+        return new TextToken(symbol.ToString());
+    }
+}
---
+++ cs/Markdown/Tokens/TagTokens/TagToken.cs
@@ -0,0 +1,14 @@
+﻿using Markdown.MarkdownTags;
+
+namespace Markdown.Tokens.TagTokens;
+
+public abstract class TagToken : IToken
+{
+    public abstract MarkdownTagType TagType { get; }
+    
+    public TokenType Type => TokenType.Tag;
+
+    public abstract string Content { get; }
+
+    public abstract bool IsTagToken(string line, int position);
+}
