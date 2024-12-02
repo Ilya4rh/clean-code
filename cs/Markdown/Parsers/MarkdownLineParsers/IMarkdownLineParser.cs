629afbad5ed45953e42d2f40994ad266f311ec41
Ilya4rh <chernykn12ilya12@gmail.com>
2024-12-03 00:57:05 +0500

    Изменение в IMarkdownLineParser

--- cs/Markdown/Parsers/MarkdownLineParsers/IMarkdownLineParser.cs
+++ cs/Markdown/Parsers/MarkdownLineParsers/IMarkdownLineParser.cs
@@ -4,5 +4,7 @@ namespace Markdown.Parsers.MarkdownLineParsers;
 
 public interface IMarkdownLineParser
 {
-    IEnumerable<IToken> ParseMarkdownLineForTokens(string markdownText);
+    IEnumerable<IToken> ParseParagraphForTokens(string markdownText);
+
+    IEnumerable<string> ParseMarkdownTextIntoParagraphs(string markdownLine);
 }
