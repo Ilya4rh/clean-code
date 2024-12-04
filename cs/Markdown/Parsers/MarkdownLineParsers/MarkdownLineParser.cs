49071dc3695a11ed5136eee96257f7c756dbb475
Ilya4rh <chernykn12ilya12@gmail.com>
2024-12-05 00:56:00 +0500

    Изменение в парсере md строки

--- cs/Markdown/Parsers/MarkdownLineParsers/MarkdownLineParser.cs
+++ cs/Markdown/Parsers/MarkdownLineParsers/MarkdownLineParser.cs
@@ -1,4 +1,5 @@
-﻿using Markdown.Tokens;
+﻿using System.Text;
+using Markdown.Tokens;
 using Markdown.Tokens.CommonTokens;
 using Markdown.Tokens.TagTokens;
 
@@ -8,27 +9,52 @@ public class MarkdownLineParser : IMarkdownLineParser
 {
     public IEnumerable<IToken> ParseParagraphForTokens(string markdownLineParagraph)
     {
+        var previousTokenPosition = -1;
+        var previousTokenLength = -1;
+        
         for (var i = 0; i < markdownLineParagraph.Length; i++)
         {
+            if (i < previousTokenLength + previousTokenPosition)
+                continue;
+            
+            previousTokenPosition = i;
             var symbol = markdownLineParagraph[i];
                 
             if (TagToken.TryCreateTagToken(markdownLineParagraph, i, out var tagToken))
             {
                 yield return tagToken!;
-
-                MovePointer(tagToken!, ref i);
-                    
+                previousTokenLength = tagToken!.Content.Length;
                 continue;
             }
-                
-            yield return CommonToken.CreateCommonToken(symbol);
+
+            var commonTokenType = CommonToken.GetCommonTokenType(symbol);
+            var tokenContent = symbol.ToString();
+            
+            if (commonTokenType == TokenType.Text)
+                tokenContent += CompleteTextTokenContent(markdownLineParagraph, i + 1, commonTokenType);
+
+            var commonToken = CommonToken.CreateCommonToken(commonTokenType, tokenContent);
+            yield return commonToken;
+            previousTokenLength = commonToken.Content.Length;
         }
     }
 
-    private static void MovePointer(TagToken tagToken, ref int pointer)
+    private static string CompleteTextTokenContent(string line, int position, TokenType tokenType)
     {
-        pointer += tagToken.Content.Length - 1;
-    } 
+        var content = new StringBuilder();
+        
+        for (; position < line.Length; position++)
+        {
+            var currentTokenType = CommonToken.GetCommonTokenType(line[position]);
+            
+            if (tokenType != currentTokenType || TagToken.TryCreateTagToken(line, position, out _))
+                break;
+
+            content.Append(line[position]);
+        }
+
+        return content.ToString();
+    }
     
     public IEnumerable<string> ParseMarkdownTextIntoParagraphs(string markdownText)
     {
