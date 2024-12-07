3bf6f2c1f5163a66e2ad366d66387538c4f99f4d
Ilya4rh <chernykn12ilya12@gmail.com>
2024-12-07 13:48:17 +0500

    Сделал класс обычным

--- cs/Markdown/MarkdownTags/MarkdownTag.cs
+++ cs/Markdown/MarkdownTags/MarkdownTag.cs
@@ -2,26 +2,17 @@
 
 namespace Markdown.MarkdownTags;
 
-public abstract class MarkdownTag
+public class MarkdownTag
 {
-    // // Тип markdown тэга
-    // public abstract MarkdownTagType TagType { get; }
-    // // Позиция тэга в строке
-    // public abstract int Position { get; }
-    // // Длина тэга
-    // public abstract int Length { get; }
-    public abstract TagToken TagToken { get; }
-    // Если тэг закрывающий
-    public abstract bool IsClosedTag { get; }
-
-    public static MarkdownTag? CreateMarkdownTag(TagToken tagToken, bool isClosedTag = false)
+    public MarkdownTag(TagToken token, bool isClosedTag)
     {
-        return tagToken.MarkdownTagType switch
-        {
-            MarkdownTagType.Italics => new MarkdownItalicsTag(tagToken, isClosedTag),
-            MarkdownTagType.Bold => new MarkdownBoldTag(tagToken, isClosedTag),
-            MarkdownTagType.Heading => new MarkdownHeadingTag(tagToken),
-            _ => null
-        };
+        Token = token;
+        IsClosedTag = isClosedTag;
     }
+
+    // Токен соответствующий данному тегу
+    public TagToken Token { get; }
+    
+    // Если тэг закрывающий
+    public bool IsClosedTag { get; }
 }
