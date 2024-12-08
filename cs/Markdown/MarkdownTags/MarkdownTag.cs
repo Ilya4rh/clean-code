2fd41e7417b88f1568fae3adc152201a26d55c7a
Ilya4rh <chernykn12ilya12@gmail.com>
2024-12-08 16:43:02 +0500

    Изменение в классе md тега

--- cs/Markdown/MarkdownTags/MarkdownTag.cs
+++ cs/Markdown/MarkdownTags/MarkdownTag.cs
@@ -4,12 +4,15 @@ namespace Markdown.MarkdownTags;
 
 public class MarkdownTag
 {
-    public MarkdownTag(TagToken token, bool isClosedTag)
+    public MarkdownTag(TagToken token, MarkdownTagType tagType, bool isClosedTag = false)
     {
         Token = token;
+        TagType = tagType;
         IsClosedTag = isClosedTag;
     }
 
+    public MarkdownTagType TagType { get; }
+    
     // Токен соответствующий данному тегу
     public TagToken Token { get; }
     
