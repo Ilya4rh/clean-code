4684a647795000f1b52e2e7c45f023422689a063
Ilya4rh <chernykn12ilya12@gmail.com>
2024-12-14 14:00:24 +0500

    Тест на линейность

--- cs/MarkdownTests/MarkdownRenderTests.cs
+++ cs/MarkdownTests/MarkdownRenderTests.cs
@@ -1,9 +1,11 @@
-﻿using FluentAssertions;
+﻿using System.Diagnostics;
+using System.Text;
+using FluentAssertions;
 using Markdown;
 using Markdown.Converters;
-using Markdown.MarkdownTags;
-using Markdown.Parsers;
-using Moq;
+using Markdown.MarkdownTagValidators;
+using Markdown.Parsers.MarkdownLineParsers;
+using Markdown.Parsers.TokensParsers;
 using NUnit.Framework;
 
 namespace MarkdownTests;
@@ -11,62 +13,49 @@ namespace MarkdownTests;
 [TestFixture]
 public class MarkdownRenderTests
 {
+    private const double ExpectedExecutionTimeOnSymbolInMilliseconds = 0.0025;
+    private readonly MarkdownLineParser mdLineParser = new();
+    private readonly HtmlConverter htmlConverter = new();
+    private readonly TokensParser tokenParser = new(new MarkdownTagValidator());
     private Md md;
-    private readonly Mock<IMarkdownLineParser> parserMock = new();
-    private readonly Mock<IHtmlConverter> converterMock = new();
-
+    
     [OneTimeSetUp]
     public void OneTimeSetup()
     {
-        md = new Md(parserMock.Object, converterMock.Object);
+        md = new Md(mdLineParser, htmlConverter, tokenParser);
     }
-
-    [Test]
-    public void Render_ShouldReturnConvertedHtml_WhenInputContainsSingleParagraph()
+    
+    [TestCase(100)]
+    [TestCase(500)]
+    public void Render_ShouldWorkOnLineal(int numberOfRepetitions)
     {
-        var markdownLine = "# Обычная строка с одним тегом";
-        var htmlLine = @"\<h1>Обычная строка с одним тегом\</h1>";
-        parserMock
-            .Setup(parser => parser.ParseMarkdownTextIntoParagraphs(It.IsAny<string>()))
-            .Returns([markdownLine]);
-        parserMock
-            .Setup(parser => parser.ParseMarkdownTags(It.IsAny<string>()))
-            .Returns([new MarkdownTag(MarkdownTagType.Heading, 0, 1)]);
-        converterMock
-            .Setup(converter => converter.Convert(It.IsAny<IEnumerable<MarkdownTag>>(), It.IsAny<string>()))
-            .Returns(htmlLine);
+        var markdownSpecText = File.ReadAllText("MdTestForTest.md");
+        var text = RepeatText(markdownSpecText);
 
-        var renderedString = md.Render(markdownLine);
+        var actualExecutionTime = GetExecutionTimeInMilliseconds(() => md.Render(text));
+        var executionTimeLimit = ExpectedExecutionTimeOnSymbolInMilliseconds * text.Length;
 
-        renderedString.Should().Be(htmlLine);
-    }
-
-    [Test]
-    public void Render_ShouldReturnConvertedHtml_WhenInputContainsSeveralParagraphs()
-    {
-        var markdownLine = "Абзац1 \n\n _Абзац2_ \n __Принадлежит2__";
-        var paragraphs = new List<string> { "Абзац1", "_Абзац2_ Принадлежит2" };
-        var tags = new List<MarkdownTag>
-            { new(MarkdownTagType.Italics, 0, 1), new(MarkdownTagType.Italics, 7, 1, true) };
-        var htmlSecondParagraph = @"\<em>_Абзац2_ Принадлежит2\</em>";
-        parserMock
-            .Setup(parser => parser.ParseMarkdownTextIntoParagraphs(It.IsAny<string>()))
-            .Returns(paragraphs);
-        parserMock
-            .Setup(parser => parser.ParseMarkdownTags(paragraphs[0]))
-            .Returns([]);
-        parserMock
-            .Setup(parser => parser.ParseMarkdownTags(paragraphs[1]))
-            .Returns(tags);
-        converterMock
-            .Setup(converter => converter.Convert(Array.Empty<MarkdownTag>(), paragraphs[0]))
-            .Returns(paragraphs[0]);
-        converterMock
-            .Setup(converter => converter.Convert(tags, paragraphs[1]))
-            .Returns(htmlSecondParagraph);
+        Console.WriteLine(actualExecutionTime);
+        Console.WriteLine(executionTimeLimit);
         
-        var renderedString = md.Render(markdownLine);
+        actualExecutionTime.Should().BeLessThanOrEqualTo(executionTimeLimit);
+    }
+    
+    private static string RepeatText(string text, int count = 1)
+    {
+        var textBuilder = new StringBuilder();
+        for (var i = 0; i < count; i++)
+            textBuilder.AppendLine(text);
 
-        renderedString.Should().Be("Абзац1\n\\<em>_Абзац2_ Принадлежит2\\</em>");
+        return textBuilder.ToString();
+    }
+    
+    private static double GetExecutionTimeInMilliseconds(Action action)
+    {
+        var stopwatch = Stopwatch.StartNew();
+        action();
+        stopwatch.Stop();
+
+        return stopwatch.ElapsedMilliseconds;
     }
 }
